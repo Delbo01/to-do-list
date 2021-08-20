611e4fc567f7b13ec67805111f3b112044eda77f
Delbo01 <leonardo.delbene@stud.unifi.it>
2021-08-20 21:24:27 +0200

    add ToDoList.h,ToDoList.cpp

---
+++ ToDoList.cpp
@@ -0,0 +1,18 @@
+//
+// Created by leonardo Del Bene on 20/08/21.
+//
+
+#include "ToDoList.h"
+
+void toDoList::removeActivity(std::unique_ptr<Activity> a) {
+    list.remove(a);
+}
+
+void toDoList::addActivity(std::unique_ptr<Activity> a) {
+    list.push_back(a);
+}
+
+void toDoList::clearAll() {
+    list.clear();
+}
+
---
+++ ToDoList.h
@@ -0,0 +1,26 @@
+//
+// Created by leonardo Del Bene on 20/08/21.
+//
+
+#ifndef TO_DO_LIST_TODOLIST_H
+#define TO_DO_LIST_TODOLIST_H
+#include "Activity.h"
+#include <list>
+#include <string>
+
+class toDoList{
+private:
+    std::list<std::unique_ptr<Activity>> list;
+    std::string title;
+public:
+    explicit toDoList(std::string t):title(t){}
+    ~toDoList()=default;
+
+    void addActivity(std::unique_ptr<Activity> a);
+    void removeActivity(std::unique_ptr<Activity> a);
+    void clearAll();
+};
+
+
+
+#endif //TO_DO_LIST_TODOLIST_H
