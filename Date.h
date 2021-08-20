3a0e3aa45d34d73a4443b422ef36d06bdf8ae2d4
Delbo01 <leonardo.delbene@stud.unifi.it>
2021-08-20 13:50:42 +0200

    add Date.cpp

--- Date.h
+++ Date.h
@@ -22,75 +22,30 @@ public:
         return year;
     }
 
-    void setYear(int year) {
-        if (year<0)
-            throw (std::out_of_range("OUT OF RANGE"));
-        else
-            Date::year = year;
-    }
+    void setYear(int year);
 
     int getMonth() const {
         return month;
     }
 
-    void setMonth(int month) {
-        if (month<=0 || month>=13)
-            throw std::out_of_range("OUT OF RANGE");
-        else
-            Date::month = month;
-    }
+    void setMonth(int month);
 
     int getDay() const {
         return day;
     }
 
-    void setDay(int day) {
-        if (month==1||month==3||month==5||month==7||month==8||month==10||month==12){
-            if (day<=0 || day>=32)
-                throw (std::out_of_range("OUT OF RANGE"));
-            else
-                Date::day = day;
-        }
-        else if (month==4||month==6||month==9||month==11) {
-            if (day <= 0 || day >= 31)
-                throw std::out_of_range("OUT OF RANGE");
-            else
-                Date::day = day;
-        } else if (month==2){
-            if ((year%4)==0){
-                if (day<=0|| day>=30)
-                    throw (std::out_of_range("OUT OF RANGE"));
-                else
-                    Date::day = day;
-            }
-            else if (day<=0|| day>=29){
-                throw (std::out_of_range("OUT OF RANGE"));
-            }
-            else
-                Date::day = day;
-        }
-    }
+    void setDay(int day);
 
     int getHour() const {
         return hour;
     }
 
-    void setHour(int hour) {
-        if (hour<0 || hour>23)
-            throw (std::out_of_range("OUT OF RANGE"));
-        else
-            Date::hour=hour;
-    }
+    void setHour(int hour);
 
     int getMinute() const {
         return minute;
     }
 
-    void setMinute(int minute) {
-        if (minute<0 || minute>59)
-            throw (std::out_of_range("OUT OF RANGE"));
-        else
-            Date::minute = minute;
-    }
+    void setMinute(int minute);
 };
 #endif //DATE_DATE_H
