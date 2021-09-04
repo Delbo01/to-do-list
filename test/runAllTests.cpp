a5dc5b5cb42a93575566bd58ff0d028a0d7bf2df
Delbo01 <leonardo.delbene@stud.unifi.it>
2021-09-04 17:40:33 +0200

    add CMakeLists.txt per unit testing

---
+++ test/runAllTests.cpp
@@ -0,0 +1,8 @@
+//
+// Created by leonardo Del Bene on 04/09/21.
+//
+#include "gtest/gtest.h"
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
